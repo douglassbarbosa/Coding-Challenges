399c9b51f95288af9723a3eafb8517c79c686f6a
FUNCIONAL\douglas.barbosa <douglas.barbosa@funcionalcorp.com.br>
2024-11-10 20:48:10 -0300

    feat: Start project base to test C#

---
+++ Beecrowd/CSharpConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System
+
+namespace Beecrowd.CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello World
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
